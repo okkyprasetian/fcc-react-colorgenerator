95b23d61cefdc999d2c099989c636ae913174c47
okkyprasetian <okkyprasetian@gmail.com>
2021-12-01 07:54:37 +0700

    set initial state and make form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Values from 'values.js'
 
 
 function App() {
+
+  // State
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  // SetState
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('haohao')
+  }
+
   return (
     <div className="App">
-      
+      <div className="header">
+        <h1>Color Generator</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" className="input" placeholder="#af30ee" onChange={e => setColor(e.target.value)} />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </div>
+      <div className="main">
+      </div>
     </div>
   );
 }
